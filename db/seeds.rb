9229c3a6ca110dfa6cf0ada1c9b81b0cb9ccfb8b
Leo Chan <lchan168@hotmail.com>
2019-06-04 10:47:13 +0800

    updated allocation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,3 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-coin_list = %w(BTC ETH XRP EOS BitcoinCash LTC ETHclassic ZCash Monero)
-puts "creating coins"
-coin_list.each do |coin|
-  Coin.create(
-    name: coin,
-    symbol: coin,
-    current_price: rand(1000..8000),
-    is_base_coin: false
-  )
-end
-
-puts "creating portfolio"
-Portfolio.create(
-  rebalance_freq: "weekly",
-  next_rebalance_dt: Date.today,
-  user_id: 1,
-  current_value: rand(1000..8000),
-  coin_id: 1
-  )
-# Portfolio.create(rebalance_freq: "weekly", next_rebalance_dt: Date.today, user_id: 1, current_value: rand(1000..8000), coin_id: 1)
