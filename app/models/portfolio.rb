64bf2b0831536585c7d69c7aa13d1a461aae4ef7
Daniele Deltodesco <deaddread666@gmail.com>
2019-06-04 15:14:23 +0800

    working on allocations form page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,7 @@ class Portfolio < ApplicationRecord
   belongs_to :user
   has_one :coin
   has_many :positions
+  has_many :allocations
 
   validates :rebalance_freq, inclusion: { in: %w[Daily Weekly Biweekly Monthly Quarterly] }
   validates :rebalance_freq, :coin_id, presence: true
