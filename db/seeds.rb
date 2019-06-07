dad675dcfc42d30932f8e140ccfacebe37a888d6
Daniele Deltodesco <deaddread666@gmail.com>
2019-06-07 16:38:29 +0800

    removed prybyebug from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 require 'open-uri'
 require 'json'
-require 'pry-byebug'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
