22efc284bb1cd3993d8c72527a6890f366a45471
Daniele Deltodesco <deaddread666@gmail.com>
2019-07-26 16:44:03 +0200

    set api keys as variables manually in each job

--- app/jobs/get_positions_job.rb
+++ app/jobs/get_positions_job.rb
@@ -4,9 +4,13 @@ require_relative '../models/portfolio'
 class GetPositionsJob < ApplicationJob
   queue_as :default
 
+
+
   def perform
     portfolios = Portfolio.all
     portfolios.each do |portfolio|
+      Binance::Api::Configuration.api_key = portfolio.user.api_key
+      Binance::Api::Configuration.secret_key = portfolio.user.secret_key
       portfolio.update_positions
     end
   end
