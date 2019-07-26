83dfdbafc2dbbf23fd2bd0702f13b55614650e1f
Daniele Deltodesco <deaddread666@gmail.com>
2019-07-26 12:13:48 +0200

    changed seed file to use binance api, as other api has too low limits

--- db/migrate/20190726091300_add_lockbox_to_private_key.rb
+++ db/migrate/20190726091300_add_lockbox_to_private_key.rb
@@ -1,5 +1,5 @@
 class AddLockboxToPrivateKey < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :privkey_ciphertext, :text
+    add_column :users, :secret_key_ciphertext, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_091300) do
     t.string "last_name"
     t.string "api_key"
     t.string "secret_key"
-    t.text "privkey_ciphertext"
+    t.text "secret_key_ciphertext"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
